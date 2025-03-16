e204f3b53c52664c93f57521b3c9418e0abe3168
developerMohib <mohibullahmohim2020@gmail.com>
2025-03-17 01:33:31 +0600

    home page added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AboutComponent } from './pages/about/about.component';
 import { ContactComponent } from './pages/contact/contact.component';
+import { HomeComponent } from './pages/home/home.component';
 
 // export const routes: Routes = [];
 export const routes: Routes = [
+  { path: '', component: HomeComponent },
   { path: 'about', component: AboutComponent },
   { path: 'contact', component: ContactComponent },
-  { path: '', redirectTo: '/about', pathMatch: 'full' }, // Default route
+  { path: '', redirectTo: '/', pathMatch: 'full' },
 ];
 
 @NgModule({
